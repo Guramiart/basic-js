1fc6ecb944f70ca8c80c107ff5d3fa79dd05d9c3
Artem Pasko <guramibox777@gmail.com>
2022-09-12 14:37:04 +0300

    feat: add vigenere-cipher solution

--- src/vigenere-cipher.js
+++ src/vigenere-cipher.js
@@ -20,13 +20,63 @@ const { NotImplementedError } = require('../extensions/index.js');
  * 
  */
 class VigenereCipheringMachine {
-  encrypt() {
-    throw new NotImplementedError('Not implemented');
-    // remove line with error and write your code here
+ 
+  constructor(isDirect = true) {
+    this.isDirect = isDirect;
+    this.alphabet = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ';
+    this.square = this.initSquare();
   }
-  decrypt() {
-    throw new NotImplementedError('Not implemented');
-    // remove line with error and write your code here
+
+  initSquare() {
+    const alphabet = this.alphabet;
+    let resultSquare = [];
+    for(let i = 0; i < alphabet.length; i++) {
+      resultSquare.push(alphabet.slice(i).concat(alphabet.slice(0, i))); 
+    }
+    return resultSquare;
+  }
+
+  encrypt(message, key) {
+    return this.cryptAction(message, key, true);
+  }
+
+  decrypt(message, key) {
+    return this.cryptAction(message, key, false);
+  }
+
+  cryptAction(message, key, action) {
+    if(!message || !key) {
+      throw new Error("Incorrect arguments!");
+    }
+
+    message = message.toUpperCase();
+    key = key.toUpperCase();
+
+    while (key.length < message.length) {
+      key += key;
+    }
+
+    let cryptResult = '';
+    let keyIndex = 0;
+
+    for (let i = 0; i < message.length; i++) {
+      if (message[i] !== ' ' && this.alphabet.includes(message[i])) {
+        if (action) {
+          cryptResult += this.square[this.alphabet.indexOf(message[i])][this.alphabet.indexOf(key[keyIndex])];
+        } else {
+          cryptResult += this.alphabet[this.square[this.alphabet.indexOf(key[keyIndex])].indexOf(message[i])];
+        }
+        keyIndex++;
+      } else {
+        cryptResult += message[i];
+      }
+    }
+   
+    if (!this.isDirect) {
+      return cryptResult.split('').reverse().join('');
+    }
+
+    return cryptResult;
   }
 }
 
